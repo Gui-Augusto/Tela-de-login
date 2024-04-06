8d24f04cff50aca0d7047f7315aecd03a362fe1e
Guilherme <guilhermeaugustomarinho1@gmail.com>
2024-04-06 12:11:53 -0300

    Ajustes

--- app/src/main/java/com/example/myapplication/CadastroActivity.kt
+++ app/src/main/java/com/example/myapplication/CadastroActivity.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,22 +17,30 @@ class CadastroActivity : AppCompatActivity() {
         val userNameID = findViewById<EditText>(R.id.etCadastroUsername)
         val senhaID = findViewById<EditText>(R.id.etCadastroSenha)
         val confirmSenhaID = findViewById<EditText>(R.id.etCadastroConfirmacao)
-
+        val btVoltar = findViewById<ImageView>(R.id.btVoltar)
         val btCadastrar = findViewById<Button>(R.id.btCadastrar)
 
         btCadastrar.setOnClickListener{
-            var nome = nomeID.text.toString()
-            var userName = userNameID.text.toString()
-            var senha = senhaID.text.toString()
-            var confirmSenha = confirmSenhaID.text.toString()
+            val nome = nomeID.text.toString()
+            val userName = userNameID.text.toString()
+            val senha = senhaID.text.toString()
+            val confirmSenha = confirmSenhaID.text.toString()
 
             if(nome != null && userName != null && senha != null && senha == confirmSenha){
-                var user = UserModel()
+                val user = UserModel()
                 user.nome = nome
                 user.username = userName
                 user.senha = senha
                 BD.users.add(user)
+                Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+                val newIntent = Intent(this, MainActivity::class.java)
+                startActivity(newIntent)
             }
+        }
+
+        btVoltar.setOnClickListener{
+            val newIntent = Intent(this, MainActivity::class.java)
+            startActivity(newIntent)
         }
     }
 }
